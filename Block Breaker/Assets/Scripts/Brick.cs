6f5c6b39e1aecff830e6527789d8f50b1d00cd1e
josercolon <josercolon@hotmail.com>
2015-07-23 00:40:31 -0400

    Brick Breaker Changes: win mechanic created. (Breakable Count) brick and Level Manager Scripts

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -5,28 +5,39 @@ public class Brick : MonoBehaviour {
 	
 	private int timesHit;
 	private LevelManager levelManager;
+	private bool isBreakable;
 
+	public static int breakableCount = 0;
 	public Sprite[] hitSprites;
 
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		// Keep track of brrakable briks
+		if (isBreakable){
+			breakableCount++;
+		}
+
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
 		}
 	void OnCollisionEnter2D (Collision2D collision) {
-		bool isBreakable = (this.tag == "Breakable");
-		HandleHits();
+		if(isBreakable){
+			HandleHits();
+		}
 	}
 	
 	void HandleHits(){
 		timesHit++;
 		int maxHits = hitSprites.Length + 1;
 		if (timesHit >= maxHits) {
+			breakableCount--;
+			levelManager.BrickDestroyed();
 			Destroy (gameObject);
 		} else {
 			LoadSprites();
@@ -38,11 +49,9 @@ public class Brick : MonoBehaviour {
 		if (hitSprites[spriteIndex]){
 			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
 		}
-
 	}
 	// NOTE:   Remove this after we have win mechanic
 	void SimulateWin() {
 		levelManager.LoadNextLevel ();
 	}
-
 }
